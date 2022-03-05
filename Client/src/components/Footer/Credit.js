e69ab895a90b9bd547614ee90cfa40a6a4b485ca
AoNoAdmiral <90148066+AoNoAdmiral@users.noreply.github.com>
2022-03-05 19:53:03 +0700

    X
    A

--- Client/src/components/Footer/Credit.js
+++ Client/src/components/Footer/Credit.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Credit() {
     return (
-        <div className="credit"> A Project By <span>Group 10</span> </div>
+        <div className="credit"> A Project By <span>Group 8</span> </div>
     )
 }
 
