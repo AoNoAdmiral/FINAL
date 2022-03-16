d0cb08ac7f09e75a6cf7e9374e7ba52d3d815cc7
AoNoAdmiral <90148066+AoNoAdmiral@users.noreply.github.com>
2022-03-16 13:25:33 +0700

    X
    A

--- Client/src/components/Data.js
+++ Client/src/components/Data.js
@@ -1,5 +1,4 @@
 export const Data = [
-    {1: "women's Fashion",2: 'image/banner1.jpg', 3: "upto 50% off", 4:"female"},
-    {1: "men's Fashion",2: 'image/banner2.jpg', 3: "upto 50% off",4:"men"},
-    {1: "kid's Fashion",2: 'image/banner3.jpg', 3: "upto 50% off",4:"kids"}
+    {1: "Tropical Fruit",2: 'image/banner1.jpg', 3: "upto 50% off", 4:"red"},
+    {1: "Fresh Fruit",2: 'image/banner2.jpg', 3: "upto 50% off",4:"green"},
 ];
