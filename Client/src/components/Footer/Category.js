d2a1b57a5c97811736b4e08ec5fd9214f9bd98f5
AoNoAdmiral <90148066+AoNoAdmiral@users.noreply.github.com>
2022-03-22 00:42:41 +0700

    X
    A

--- Client/src/components/Footer/Category.js
+++ Client/src/components/Footer/Category.js
@@ -4,8 +4,8 @@ function Category() {
     return (
         <div className="box">
         <h3>category</h3>
-        <a href="/#products" className="filterLink" data-filter="men"> <i className="fas fa-arrow-right"></i> men </a>
-        <a href="/#products" className="filterLink" data-filter="female"> <i className="fas fa-arrow-right"></i> women </a>
+        <a href="/#products" className="filterLink" data-filter="men"> <i className="fas fa-arrow-right"></i> Fruit </a>
+        <a href="/#products" className="filterLink" data-filter="female"> <i className="fas fa-arrow-right"></i> Vegetable </a>
         <a href="/#products" className="filterLink" data-filter="featured"> <i className="fas fa-arrow-right"></i> Featured </a>
         <a href="/#products" className="filterLink" data-filter="seller"> <i className="fas fa-arrow-right"></i> best seller </a>
         <a href="/#products" className="filterLink" data-filter="arrivals"> <i className="fas fa-arrow-right"></i> new arrivals </a>
